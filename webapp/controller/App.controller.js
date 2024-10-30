cf827069cd9064ef46a6cbd9c0812fcb00b4492c
Goutam Saha <goutam.saha@mt.com>
2024-10-30 08:43:43 +0000

    my fisr stable app

---
+++ webapp/controller/App.controller.js
@@ -0,0 +1,9 @@
+sap.ui.define([
+    "mt/fin/ap/gs/controller/BaseController"
+], function(Controller) {
+    'use strict';
+    return Controller.extend("mt.fin.ap.gs.controller.App", {
+       
+    })
+   
+});
